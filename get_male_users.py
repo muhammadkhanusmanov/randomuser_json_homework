2b4ef24d847b5df67996b0a7dbcb159e7fda8916
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-11-23 11:59:45 -0600

    02

--- get_male_users.py
+++ get_male_users.py
@@ -1,3 +1,4 @@
+from from_json import read_json as rj
 def get_male_users(data:dict)->list:
     """Gets all male users from the data
     Args:
@@ -5,4 +6,10 @@ def get_male_users(data:dict)->list:
     Returns:
         list: A list of users
     """
-    pass
+    a=[]
+    for i in data['users']:
+        if i['gender']=='male':
+            a.append(i)
+    return a 
+data=rj('users.json')
+print(get_male_users(data))
